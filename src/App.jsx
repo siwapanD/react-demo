dbc4917a95e41220264f883712ed078796de69d4
siwapanD <siwapandev@gmail.com>
2026-01-14 10:28:36 +0700

    Add 'Thailand' heading to App component

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello World</h1>
+      <h1>Thailand</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
